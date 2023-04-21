e891441f662ac1c5b0add98cfa95f50d7d91eb4c
Maria Ciovacco Juarez <mariaciovaccojuarez@MBP-Maria.lan>
2023-04-21 11:37:00 -0500

    added express and paths

--- server.js
+++ server.js
@@ -8,3 +8,29 @@ const uuid = require('./helpers/uuid');
 const PORT = 3001;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const readFromFile = async (filePath) => {
+  try {
+    const data = await readFileAsync(filePath, 'utf8');
+    return JSON.parse(data);
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
+};
